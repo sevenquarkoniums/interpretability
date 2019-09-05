5282779731c9bef8bdf7eda03fa6eb74d4cca83f
sevenquarkoniums <sevenquarkoniums@gmail.com>
2019-09-05 14:46:03 -0400

    small

--- setup.sh
+++ setup.sh
@@ -1,1 +1,9 @@
+git config --global user.email 'sevenquarkoniums@gmail.com'
+git config --global user.name 'sevenquarkoniums'
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=86400'
+git clone https://github.com/sevenquarkoniums/testProject.git
+cp testProject/.vimrc ~/.vimrc
+git clone https://github.com/sevenquarkoniums/interpretability.git
+cd interpretability
 wget http://files.fast.ai/data/imagenet-sample-train.tar.gz
