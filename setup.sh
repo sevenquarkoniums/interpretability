ac555b23b1087dd3d0aedc2fff54dcfa70b89b6d
sevenquarkoniums <sevenquarkoniums@gmail.com>
2019-09-09 17:02:04 -0400

    setup more

--- setup.sh
+++ setup.sh
@@ -6,7 +6,21 @@ git clone https://github.com/sevenquarkoniums/testProject.git
 cp testProject/.vimrc ~/.vimrc
 git clone https://github.com/sevenquarkoniums/interpretability.git
 cd interpretability
+
 wget http://files.fast.ai/data/imagenet-sample-train.tar.gz
+tar xzf imagenet-sample-train.tar.gz
+rm -f imagenet-sample-train.tar.gz
+mkdir data
+mv train data/all
+mkdir data/train
+mkdir data/val
+chmod +x moveData.py
+chmod +x imagenet.py
+./moveData.py
+
+./imagenet.py -a alexnet -j 16 data
+
+./imagenet.py -a alexnet -j 16 --start-epoch 90 --epochs 1000 --resume checkpoint.pth.tar data
 
 # to delete
-gcloud deployment-manager deployments delete name
+gcloud deployment-manager deployments delete imagenet
